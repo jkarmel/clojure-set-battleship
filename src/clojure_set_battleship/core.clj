76dd5bfe69a16c8a640869e71ec1bcc25873cba3
Jeremy Karmel <jeremy@hired.com>
2015-02-15 12:05:19 -0800

    create map of coordinates to ship-ids

--- src/clojure_set_battleship/core.clj
+++ src/clojure_set_battleship/core.clj
@@ -5,3 +5,11 @@
     (for [delta (range 0 size)]
       {:x (+ x (* dx delta))
         :y (+ y (* dy delta))})))
+
+(defn id+coords=>coords->id [id coords]
+  (reduce #(assoc %1 %2 id) {} coords))
+
+(defn ship-placements=>points->ship-id [placements]
+  (let [id-coords-pairs
+        (map-indexed (fn [ship-id placement] [ship-id (apply ship->points placement)]) placements)]
+    (apply merge (map #(apply id+coords=>coords->id %) id-coords-pairs))))
--- test/clojure_set_battleship/core_test.clj
+++ test/clojure_set_battleship/core_test.clj
@@ -6,3 +6,8 @@
   (testing "a ship at 0 0"
     (is (= (ship->points 3 {:x 0 :y 0} {:x 1 :y 0})
            #{{:x 0 :y 0} {:x 1 :y 0} {:x 2 :y 0}}))))
+
+(deftest test-ship-placements=>points->ship-id
+  (testing "place"
+    (is (= (ship-placements=>points->ship-id [[2 {:x 0 :y 0} {:x 0 :y 1}]])
+           {{:x 0 :y 0} 0 {:x 0 :y 1} 0}))))
